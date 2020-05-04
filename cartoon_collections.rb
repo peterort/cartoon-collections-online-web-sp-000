63fe3a1dc465bb67bb6c311f14f0283c71a4efcc
Peter Ort <peter@peterort.com>
2020-05-04 17:01:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planteer_calls)
-  planteer_calls.map {|string| string.capitalize}
+  planteer_calls.map {|string| string.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
