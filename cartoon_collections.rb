1cac7f435cb905db1c6085136a93b0a69c5f30c3
Peter Ort <peter@peterort.com>
2020-05-04 17:03:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planteer_calls)
   planteer_calls.map {|string| string.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 
