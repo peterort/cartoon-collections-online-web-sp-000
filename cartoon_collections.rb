cdb814f036a332c618cc3a601364252176c55485
Peter Ort <peter@peterort.com>
2020-05-04 17:19:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,6 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.include? cheese_types
+  found_cheese = []
+  strings.include? found_cheese << cheese_types
 end
